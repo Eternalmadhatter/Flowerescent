ca5eeb0e6e4380c75fc200ecd3951032aa0c5ef6
Eternalmadhatter <97937707+Eternalmadhatter@users.noreply.github.com>
2022-11-08 00:34:40 +0800

    Update MASTER_CODE.ino

--- MASTER_CODE/MASTER_CODE.ino
+++ MASTER_CODE/MASTER_CODE.ino
@@ -95,14 +95,14 @@ void loop() {
   }
    avgDist = totaldist/5;
    printToScreen(avgDist);*/
-  if(force0<499 && force1<499 && force2<499) 
+  if(force0<499 && force1<499 && force2<499)//if no one step 
   {
    
-   for(int i = 0; i<3;i++)
-   {
-      trig[i] = 0; 
-   }
-   randomColors();
+     for(int i = 0; i<3;i++)
+     {
+        trig[i] = 0; 
+     }
+     randomColors();
    
   }
   else
@@ -132,9 +132,10 @@ void loop() {
       trig[2]=0;
     }
   }
-  if(trig[0] == trigPrv[0] && trig[1] == trigPrv[1] && trig[2] == trigPrv[2])
+  
+  if(trig[0] == trigPrv[0] && trig[1] == trigPrv[1] && trig[2] == trigPrv[2])//compair to previous input 
   {
-    for( int i = 0 ; i<3 ; i++)
+    for( int i = 0 ; i<3 ; i++)//saves current input into prev input 
     {
       trigPrv[i] = trig[i];
     }
@@ -142,12 +143,12 @@ void loop() {
   }
   else
   {
-    for( int i = 0 ; i<3 ; i++)
+    for( int i = 0 ; i<3 ; i++)//saves current input into prev input 
     {
       trigPrv[i] = trig[i];
     }
 
-    if(trig[2] == 1)
+    if(trig[2] == 1)//priority to last stone
     {
         Wire.beginTransmission(9); // transmit to device #9
         Wire.write(trig[2]);              // sends 0  
@@ -216,68 +217,186 @@ void randomColors()
     }
   }
   delay(300);
-  for (int j = 0; j <= 20; j++)//clears LEDs for the nest set
-  {
-    leds[randomColor[j]] = CRGB(0,0 ,0); //Rossa
-  }
+  FastLED.clear();//clear all leds
   FastLED.show();
 }
 
 void BuPuFlowers()
 {
-  FastLED.clear();
+  FastLED.clear();//clear all leds
   FastLED.show();
-  for(countUp==0;countUp<=NUM_LEDS; countUp++)
-  {
-    if (countUp%2 == 0)
-    {
-      leds[countUp] = CRGB(10, 10, 255); //blue
+  
+      leds[9] = CRGB(10, 10, 255); //blu
+      leds[10] = CRGB(50, 10, 200); //purple;
+      leds[11] = CRGB(10, 10, 255); //blu
+      leds[25] = CRGB(50, 10, 200); //purple;
+      leds[26] = CRGB(10, 10, 255); //blu
+      leds[26] = CRGB(50, 10, 200); //purple;
+      leds[27] = CRGB(10, 10, 255); //blu
+      leds[28] = CRGB(50, 10, 200); //purple;
+      leds[29] = CRGB(10, 10, 255); //blu
+      leds[30] = CRGB(50, 10, 200); //purple;
+      leds[27] = CRGB(10, 10, 255); //blu
+      leds[28] = CRGB(50, 10, 200); //purple;
+      FastLED.show();//wave step 1
+      delay(100);
       
-    }
-    if (countUp%2 == 1)
-    {
-      leds[countUp] = CRGB(50, 10, 200); //purple;
-    }
-    FastLED.show();
-  }
+      leds[1] = CRGB(10, 10, 255); //blu
+      leds[2] = CRGB(50, 10, 200); //purple;
+      leds[3] = CRGB(10, 10, 255); //blu
+      leds[4] = CRGB(50, 10, 200); //purple;
+      leds[5] = CRGB(10, 10, 255); //blu
+      leds[6] = CRGB(50, 10, 200); //purple;
+      leds[7] = CRGB(10, 10, 255); //blu
+      leds[8] = CRGB(50, 10, 200); //purple;
+      leds[20] = CRGB(10, 10, 255); //blu
+      leds[24] = CRGB(50, 10, 200); //purple;
+      leds[31] = CRGB(10, 10, 255); //blu
+      leds[38] = CRGB(50, 10, 200); //purple;
+      FastLED.show();//wave step 2
+      delay(100);
+      
+      leds[12] = CRGB(10, 10, 255); //blu
+      leds[13] = CRGB(50, 10, 200); //purple;
+      leds[14] = CRGB(10, 10, 255); //blu
+      leds[15] = CRGB(50, 10, 200); //purple;
+      leds[16] = CRGB(10, 10, 255); //blu
+      leds[17] = CRGB(50, 10, 200); //purple;
+      leds[18] = CRGB(10, 10, 255); //blu
+      leds[19] = CRGB(50, 10, 200); //purple;
+      leds[21] = CRGB(10, 10, 255); //blu
+      leds[22] = CRGB(50, 10, 200); //purple;
+      leds[23] = CRGB(10, 10, 255); //blu
+      leds[32] = CRGB(50, 10, 200); //purple;
+      leds[37] = CRGB(10, 10, 255); //blu
+      leds[39] = CRGB(50, 10, 200); //purple;
+      FastLED.show();//wave step 3
+      delay(100);
+      
+      leds[34] = CRGB(10, 10, 255); //blu
+      leds[35] = CRGB(50, 10, 200); //purple;
+      leds[36] = CRGB(10, 10, 255); //blu
+      FastLED.show();//wave step 4
+      delay(100);
 }
 
 void YellowFlowers()
 {
-  FastLED.clear();
+  FastLED.clear();//clear all leds
   FastLED.show();
-  for(countUp==0;countUp<=NUM_LEDS; countUp++)
-  {
-    if (countUp%2 == 0){
-    leds[countUp] = CRGB(247, 184, 1); //selective yellow
-    }
-    
-    if (countUp%2 == 1){
-    leds[countUp] = CRGB(243, 91, 4); //tangerine
-    }
-    FastLED.show();
-    delay(50);
-  }
+  leds[1] = CRGB(247, 184, 1); //selective yellow
+  leds[2] = CRGB(243, 91, 4); //tangerine
+  leds[3] = CRGB(247, 184, 1); //selective yellow
+  leds[4] = CRGB(243, 91, 4); //tangerine  
+  leds[5] = CRGB(247, 184, 1); //selective yellow
+  leds[6] = CRGB(243, 91, 4); //tangerine
+  leds[7] = CRGB(247, 184, 1); //selective yellow
+  leds[9] = CRGB(243, 91, 4); //tangerine
+  leds[1] = CRGB(247, 184, 1); //selective yellow
+  leds[2] = CRGB(243, 91, 4); //tangerine
+  FastLED.show();//wave step 1
+  delay(100);
+  
+  leds[8] = CRGB(247, 184, 1); //selective yellow
+  leds[12] = CRGB(243, 91, 4); //tangerine
+  leds[13] = CRGB(247, 184, 1); //selective yellow
+  leds[14] = CRGB(243, 91, 4); //tangerine  
+  leds[15] = CRGB(247, 184, 1); //selective yellow
+  leds[16] = CRGB(243, 91, 4); //tangerine
+  leds[17] = CRGB(247, 184, 1); //selective yellow
+  leds[18] = CRGB(243, 91, 4); //tangerine
+  leds[20] = CRGB(247, 184, 1); //selective yellow
+  leds[24] = CRGB(243, 91, 4); //tangerine
+  leds[25] = CRGB(247, 184, 1); //selective yellow
+  leds[26] = CRGB(243, 91, 4); //tangerine
+  FastLED.show(); //wave step 2
+  delay(100);
+  
+  leds[19] = CRGB(247, 184, 1); //selective yellow
+  leds[21] = CRGB(243, 91, 4); //tangerine
+  leds[22] = CRGB(247, 184, 1); //selective yellow
+  leds[23] = CRGB(243, 91, 4); //tangerine  
+  leds[27] = CRGB(247, 184, 1); //selective yellow
+  leds[30] = CRGB(243, 91, 4); //tangerine
+  leds[31] = CRGB(247, 184, 1); //selective yellow
+  FastLED.show(); //wave step 3
+  delay(100);
+  
+  leds[28] = CRGB(243, 91, 4); //tangerine
+  leds[32] = CRGB(247, 184, 1); //selective yellow
+  leds[33] = CRGB(243, 91, 4); //tangerine
+  leds[34] = CRGB(247, 184, 1); //selective yellow
+  leds[37] = CRGB(243, 91, 4); //tangerine
+  leds[38] = CRGB(247, 184, 1); //selective yellow
+  FastLED.show(); //wave step 4 
+  delay(100);
+  
+  leds[29] = CRGB(247, 184, 1); //selective yellow
+  leds[39] = CRGB(243, 91, 4); //tangerine
+  leds[36] = CRGB(247, 184, 1); //selective yellow
+  leds[35] = CRGB(243, 91, 4); //tangerine  
+  FastLED.show(); //wave step 5
+  delay(100);
+  
 }
 
 void OrRedFlowers()
 {
   FastLED.clear();
   FastLED.show();
-  for(countUp==0;countUp<=NUM_LEDS; countUp++)
-  { 
-    if (countUp%2 == 0)
-    {
-      leds[countUp] = CRGB(220, 47, 2); //persimmon
-    }
-    
-    if (countUp%2 == 1)
-    {
-      leds[countUp] = CRGB(222,10 ,10); //Rosso corsa
-    }
-    FastLED.show();
-    delay(10);
-  }
+  leds[6] = CRGB(220, 47, 2); //persimmon
+  leds[7] = CRGB(222,10 ,10); //Rosso corsa
+  leds[8] = CRGB(220, 47, 2); //persimmon
+  leds[9] = CRGB(222,10 ,10); //Rosso corsa
+  leds[20] = CRGB(220, 47, 2); //persimmon
+  leds[21] = CRGB(222,10 ,10); //Rosso corsa
+  leds[22] = CRGB(220, 47, 2); //persimmon
+  leds[23] = CRGB(222,10 ,10); //Rosso corsa
+  FastLED.show();
+  delay(100);
+
+  leds[6] = CRGB(220, 47, 2); //persimmon
+  leds[10] = CRGB(222,10 ,10); //Rosso corsa
+  leds[17] = CRGB(220, 47, 2); //persimmon
+  leds[18] = CRGB(222,10 ,10); //Rosso corsa
+  leds[19] = CRGB(220, 47, 2); //persimmon
+  leds[24] = CRGB(222,10 ,10); //Rosso corsa
+  leds[33] = CRGB(220, 47, 2); //persimmon
+  leds[34] = CRGB(222,10 ,10); //Rosso corsa  
+  FastLED.show();
+  delay(100);
+
+  leds[1] = CRGB(220, 47, 2); //persimmon
+  leds[2] = CRGB(222,10 ,10); //Rosso corsa
+  leds[3] = CRGB(220, 47, 2); //persimmon
+  leds[4] = CRGB(222,10 ,10); //Rosso corsa
+  leds[5] = CRGB(220, 47, 2); //persimmon
+  leds[16] = CRGB(222,10 ,10); //Rosso corsa
+  leds[25] = CRGB(220, 47, 2); //persimmon
+  leds[31] = CRGB(222,10 ,10); //Rosso corsa  
+  leds[32] = CRGB(220, 47, 2); //persimmon
+  FastLED.show();
+  delay(100);
+  leds[11] = CRGB(222,10 ,10); //Rosso corsa  
+  leds[12] = CRGB(220, 47, 2); //persimmon
+  leds[13] = CRGB(222,10 ,10); //Rosso corsa
+  leds[14] = CRGB(220, 47, 2); //persimmon
+  leds[15] = CRGB(222,10 ,10); //Rosso corsa
+  leds[26] = CRGB(220, 47, 2); //persimmon
+  leds[30] = CRGB(222,10 ,10); //Rosso corsa
+  leds[35] = CRGB(220, 47, 2); //persimmon
+  leds[36] = CRGB(222,10 ,10); //Rosso corsa  
+  leds[37] = CRGB(220, 47, 2); //persimmon
+  FastLED.show();
+  delay(100);
+
+  leds[27] = CRGB(222,10 ,10); //Rosso corsa  
+  leds[28] = CRGB(220, 47, 2); //persimmon
+  leds[29] = CRGB(222,10 ,10); //Rosso corsa
+  leds[38] = CRGB(220, 47, 2); //persimmon
+  leds[39] = CRGB(222,10 ,10); //Rosso corsa
+  FastLED.show();
+  delay(100);
 }
 
 
